fa9840b34e56545c887bb2fa915b9f4b5f0dc99b
Tulio-Rangel <tjrn20@hotmail.com>
2022-05-03 17:09:17 -0500

    primer borrador del reto

--- calculadora_precios.py
+++ calculadora_precios.py
@@ -1,1 +1,12 @@
 #Calculadora de precios
+iva = 0.19
+producto = eval(input("ingrese el valor unitario del producto:"))
+cantidad = eval(input("ingrese la cantidad de productos:"))
+subtotal = producto * cantidad
+aux = str(input('Ingrese s o n:'))
+
+if aux == 's':
+    print('IVA INCLUÍDO')
+else: print('PRODUCTO SIN IVA')
+
+print('SUBTOTAL: subtotal')
