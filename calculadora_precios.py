d0662a2c64b116aede1ab662bc9485bb7e1157fb
Tulio-Rangel <tjrn20@hotmail.com>
2022-05-03 20:48:22 -0500

    segundo borrador del reto

--- calculadora_precios.py
+++ calculadora_precios.py
@@ -1,12 +1,39 @@
 #Calculadora de precios
-iva = 0.19
+iva = 1.19
 producto = eval(input("ingrese el valor unitario del producto:"))
+aux = str(input('¿El producto está gravado con IVA? Ingrese S o N:'))
 cantidad = eval(input("ingrese la cantidad de productos:"))
 subtotal = producto * cantidad
-aux = str(input('Ingrese s o n:'))
 
-if aux == 's':
+if aux == 'S':
     print('IVA INCLUÍDO')
 else: print('PRODUCTO SIN IVA')
 
-print('SUBTOTAL: subtotal')
+if aux == 'S':
+    subtotal *= iva
+
+print(f'SUBTOTAL: {subtotal}')
+
+aux2 = str(input('¿Faltan productos por cobrar? Ingrese S o N:'))
+
+while aux2 == 'S':
+    producto = eval(input("ingrese el valor unitario del producto:"))
+    aux = str(input('¿El producto está gravado con IVA? Ingrese S o N:'))
+    cantidad = eval(input("ingrese la cantidad de productos:"))
+    subtotal = producto * cantidad
+
+    if aux == 'S':
+        print('IVA INCLUÍDO')
+    else: print('PRODUCTO SIN IVA')
+
+    if aux == 'S':
+        subtotal *= iva
+
+    print(f'SUBTOTAL: {subtotal}')
+
+    aux2 = str(input('¿Faltan productos por cobrar? Ingrese S o N:'))
+
+    if aux2 == 'N':
+        break
+
+print(f'TOTAL A COBRAR: {subtotal}')
